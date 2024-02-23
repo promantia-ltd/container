b454dca19bd50646111f311ce7c96af8999a1a7c
MohanPromantia <mohan.k@promantia.com>
2024-02-23 12:31:33 +0530

    17874 Added container in item dashboard

--- container/hooks.py
+++ container/hooks.py
@@ -214,9 +214,9 @@ scheduler_events = {
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
 # along with any modifications made in other Frappe apps
-# override_doctype_dashboards = {
-# 	"Task": "container.task.get_dashboard_data"
-# }
+override_doctype_dashboards = {
+	"Item": "container.container.doctype.item.item_dashboard.get_data"
+}
 
 # exempt linked doctypes from being automatically cancelled
 #
