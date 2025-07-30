a4867aa582b0481def56a07f024f1ddb3a901ffe
Rohith <rohith.v@promantia.com>
2025-07-30 09:44:09 +0530

    26870 - Added the scrap Qty condition for 1st Stock Entry for Reserved Items.

--- container/container/doctype/stock_entry/stock_entry.js
+++ container/container/doctype/stock_entry/stock_entry.js
@@ -119,9 +119,13 @@ frappe.ui.form.on('Stock Entry', {
 												let qty=0;
 												let available_qty_use="";
 												let available_qty="";
+												let is_scrap_qty = 0;
+												if (r.message.scrap_qty == 1) {
+													is_scrap_qty = 0.1;
+												}
 												for (let i = 0; i < r.message.target.container_no.length; i++) {
 													let current_available_qty = parseFloat(r.message.target.primary_available_qty[i]);
-													if (current_available_qty > 0.1) {
+													if (current_available_qty > is_scrap_qty) {
 														container_no += String(r.message.target.container_no[i]) + ",";
 														available_qty += String(r.message.target.primary_available_qty[i]) + ",";
 														available_qty_use += String(r.message.target.primary_available_qty_used[i]) + ",";
